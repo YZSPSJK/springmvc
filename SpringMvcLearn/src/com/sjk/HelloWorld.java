bfd2e538797ac6d682664c803b4d54186245f689
YZSPSJK <yzspsjk@163.com>
2017-05-15 16:05:14 +0800

    新增项目

---
+++ SpringMvcLearn/src/com/sjk/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.sjk;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorld {
+
+    /**
+     * 1. 使用RequestMapping注解来映射请求的URL
+     * 2. 返回值会通过视图解析器解析为实际的物理视图, 对于InternalResourceViewResolver视图解析器，会做如下解析
+     * 通过prefix+returnVal+suffix 这样的方式得到实际的物理视图，然后会转发操作
+     * "/WEB-INF/views/success.jsp"
+     * @return
+     */
+    @RequestMapping("/helloworld")
+    public String hello(){
+        System.out.println("hello world");
+        return "success";
+    }
+}
---
+++ Springmvc/src/com/sjk/servlet/Test.java
@@ -0,0 +1,5 @@
+package com.sjk.servlet;
+
+public class Test {
+
+}
